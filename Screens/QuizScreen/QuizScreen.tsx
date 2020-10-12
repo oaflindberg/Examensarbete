149946966d93896db2a6aed8a22d10c92d66d516
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-10-12 10:04:19 +0200

    Fixed getting first question using index stat

--- Screens/QuizScreen/QuizScreen.tsx
+++ Screens/QuizScreen/QuizScreen.tsx
@@ -8,17 +8,19 @@ import firebase from './../../firebase/firebase'
 
 export default function QuizScreen() {
   const [index, setIndex] = useState<number>(0)
-  const [question, setQuestion] = useState({})
+  const [question, setQuestion] = useState<object>()
   const database = firebase.database()
 
   useEffect(() => {
     database
-      .ref('/questions')
+      .ref(`/questions/${index}`)
       .once('value')
       .then((dataSnapshot) => {
         setQuestion(dataSnapshot)
       })
   }, [])
+
+  // console.log(question.question)
 
   const [loaded, error] = useFonts({
     Akkurat: require('./../../assets/fonts/Akkurat.ttf'),
@@ -27,30 +29,6 @@ export default function QuizScreen() {
   if (!loaded) {
     return null
   }
-
-  const questions = [
-    {
-      question:
-        'Vilken spelare i IFK Göteborg har fått flest gula kort i Allsvenskan genom tiderna?',
-      answer: 'Sebastian Eriksson',
-      alternatives: {
-        one: 'Sebastian Eriksson',
-        two: 'Pontus Wernbloom',
-        three: 'Håkan Mild',
-        four: 'Magnus Erlingmark',
-      },
-    },
-    {
-      question: 'Vilket år grundades IFK Göteborg?',
-      answer: '1904',
-      alternatives: {
-        one: '1910',
-        two: '1901',
-        three: '1904',
-        four: '1906',
-      },
-    },
-  ]
 
   const checkAnswer = (pressed: string, answer: string) => {
     if (pressed == answer) {
@@ -65,14 +43,13 @@ export default function QuizScreen() {
     }
   }
 
-  console.log(question)
   return (
     <Layout>
       <QuestionContainer
-        questionNumber={`Fråga ${index + 1} av ${questions.length}`}
-        question={questions[index].question}
+        questionNumber={`Fråga ${index + 1}`}
+        question={'hej'}
       />
-      {Object.entries(questions[index].alternatives).map(([key, value], i) => {
+      {/* {Object.entries(questions[index].alternatives).map(([key, value], i) => {
         return (
           <Button
             key={i}
@@ -83,7 +60,7 @@ export default function QuizScreen() {
           />
         )
       })}
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
     </Layout>
   )
 }
