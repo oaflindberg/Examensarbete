3947b31da6693fef397628633ead28349264ea9c
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2020-10-12 13:23:09 +0200

    Fixed coloring of buttons on correct / wrong answer

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,7 +4,6 @@ import { StyledButton, ButtonText } from './Style'
 interface ButtonProps {
   text: any
   handleClick?: () => void
-<<<<<<< HEAD
   correct?: boolean
   wrong?: boolean
 }
@@ -15,12 +14,6 @@ export default function Button({
   wrong,
   handleClick,
 }: ButtonProps) {
-=======
-  wrong?: any
-}
-
-export default function Button({ text, handleClick, wrong }: ButtonProps) {
->>>>>>> 9e5dec66864d551cc679b7735f0d838dc0f51507
   return (
     <StyledButton correct={correct} wrong={wrong}>
       <ButtonText onPress={handleClick}>{text}</ButtonText>
